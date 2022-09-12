329dc72a98e9c641abdb678279e4b6dffa507c45
AmirGit <aeonthings@gmail.com>
2022-09-12 04:34:46 +0200

    .

--- STREAMLIT/streamlit.py
+++ STREAMLIT/streamlit.py
@@ -28,16 +28,18 @@ t = pd.to_datetime(t)
 today = datetime.strftime(datetime.now(), "%d/%m/%Y")
 
 st.set_page_config(page_title="ASTROTOOL")
-st.write("ASTROTOOL")
+st.markdown('**ASTROTOOL**')
+
+# ONGLETS METHODS
 
 m1 = Nat.copy()
 m2 = Spi.copy()
-m3 = TrNa.copy()
-m4 = TrTr.copy()
+m3 = TrTr.copy()
+m4 = TrNa.copy()
 m5 = addPrice.copy()
 m6 = Fib.copy()
 
-o1,o2,o3,o4,o5,o6 = st.tabs(["Natural Square", "Spiral", "TrNa", "TrTr", "addPrice", "Fib"])
+o1,o2,o3,o4,o5,o6 = st.tabs(["NatSq", "Spiral", "TrTr", "TrNa", "addPrice", "Fib"])
 
 with o1:
     col1, col2 = st.columns([1,3.33])
@@ -45,7 +47,8 @@ with o1:
     with col1:
         st.dataframe(m1.style.background_gradient(cmap='Blues'))
     with col2:
-        fig1 = px.bar(m1, x='Date', y='Hit')
+        fig1 = px.bar(m1, x='Date', y='Hit', color='Hit', color_continuous_scale=px.colors.sequential.Blues,
+                 title="NatSq | Hits Chart")
         st.plotly_chart(fig1, use_container_width=True)
 
 with o2:
@@ -54,7 +57,8 @@ with o2:
     with col1:
         st.dataframe(m2.style.background_gradient(cmap='Blues'))
     with col2:
-        fig2 = px.bar(m2, x='Date', y='Hit')
+        fig2 = px.bar(m2, x='Date', y='Hit', color='Hit', color_continuous_scale=px.colors.sequential.Blues,
+                 title="Spiral | Hits Chart")
         st.plotly_chart(fig2, use_container_width=True)
 
 with o3:
@@ -63,7 +67,8 @@ with o3:
     with col1:
         st.dataframe(m3.style.background_gradient(cmap='Blues'))
     with col2:
-        fig3 = px.bar(m3, x='Date', y='Points')
+        fig3 = px.bar(m3, x='Date', y='Points',
+                 title="TrTr | Hits Chart")
         st.plotly_chart(fig3, use_container_width=True)
 
 with o4:
@@ -72,7 +77,8 @@ with o4:
     with col1:
         st.dataframe(m4.style.background_gradient(cmap='Blues'))
     with col2:
-        fig4 = px.bar(m4, x='Date', y='Points')
+        fig4 = px.bar(m4, x='Date', y='Points',
+                 title="TrNa | Hits Chart")
         st.plotly_chart(fig4, use_container_width=True)
 
 with o5:
@@ -81,7 +87,7 @@ with o5:
     with col1:
         st.dataframe(m5.style.background_gradient(cmap='Blues'))
     with col2:
-        fig5 = px.bar(m5, x='Date', y='Hit')
+        fig5 = px.bar(m5, x='Date', y='Hit', title="addPrice | Hits Chart")
         st.plotly_chart(fig5, use_container_width=True)
 
 with o6:
@@ -90,5 +96,6 @@ with o6:
     with col1:
         st.dataframe(m6.style.background_gradient(cmap='Blues'))
     with col2:
-        fig6 = px.bar(m6, x='Date', y='Hit')
+        fig6 = px.bar(m6, x='Date', y='Hit', color='Hit', color_continuous_scale=px.colors.sequential.Blues,
+                 title="Fib | Hits Chart")
         st.plotly_chart(fig6, use_container_width=True)
