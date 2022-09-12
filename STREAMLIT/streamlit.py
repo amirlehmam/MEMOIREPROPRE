123b282a08c762e4d9e103933ed7729e1b564a02
AmirGit <aeonthings@gmail.com>
2022-09-12 16:57:38 +0200

    .

--- STREAMLIT/streamlit.py
+++ STREAMLIT/streamlit.py
@@ -58,7 +58,7 @@ file_path = Path(__file__).parent / "hashed_pw.pkl"
 with file_path.open("rb") as file:
     hashed_passwords = pickle.load(file)
 
-authenticator = stauth.Authenticate(names, usernames, hashed_passwords, "app_home", "abcdef", cookie_expiry_days=3)
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords, "app_home", "abcdefg", cookie_expiry_days=3)
 
 names, authentication_status, username = authenticator.login("Login", "main")
 
