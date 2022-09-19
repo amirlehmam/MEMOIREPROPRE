99064f690012eb2ee6ec14b7c32206e94e43364a
AmirGit <aeonthings@gmail.com>
2022-09-19 16:15:01 +0200

    .

--- Aspects.py
+++ Aspects.py
@@ -38,32 +38,7 @@ today = pd.to_datetime(today)
 # **III. ASPECTS TABLE & TOOLS.py**
 ### **III.1 Aspects H/G TrTr & TrNa**
 #### **III.1.1 HELIO TrTr & TrNa**
-# HELIO ASPECT TrTr - MADE BY US AUTOMATICALY FINDS INTO DATA
 
-#ha = helio_asp
-#col = []
-#cols = ha.columns[1:]
-#df = ha[cols]
-#Date = list[ha['Date']]
-
-#for columns in df.columns:
-
-    #globals()["liste_%s"%columns] = []
-
-    #for row, i in enumerate(df[columns]):
-        #if (i >= 0 and i <= 0.03) or (i >= 3.75 and i <= 3.755) or (i >= 7.5 and i <= 7.55) or (i >= 15 and i <= 15.15) or (i >= 22.5 and i <= 22.55) or (i >= 30 and i <= 30.15) or (i >= 36 and i <= 36.15) or (i >= 45 and i <= 45.15) or (i >= 60 and i <= 60.15) or (i >= 72 and i <= 72.15) or (i >= 90 and i <= 90.15) or (i >= 120 and i <= 120.15) or (i >= 135 and i <= 135.15) or (i >= 144 and i <= 144.15) or (i >= 150 and i <= 150.15) or (i >= 180 and i <= 180.15):
-            #globals()["liste_%s"%columns].append(i)
-        #else:
-            #globals()["liste_%s"%columns].append(" ")
-
-#flu = helio_asp[['Date']].copy()
-#dict = {'Sun_Mer': liste_Sun_Mer, 'Sun_Ven': liste_Sun_Ven, 'Sun_Mar': liste_Sun_Mar, 'Sun_Jup': liste_Sun_Jup, 'Sun_Sat': liste_Sun_Sat, 'Sun_Ura': liste_Sun_Ura, 'Sun_Nep': liste_Sun_Nep, 'Sun_Plu': liste_Sun_Plu, 'Mer_Ven': liste_Mer_Ven, 'Mer_Mar': liste_Mer_Mar, 'Mer_Jup': liste_Mer_Jup, 'Mer_Sat': liste_Mer_Sat, 'Mer_Ura': liste_Mer_Ura, 'Mer_Nep': liste_Mer_Nep, 'Mer_Plu': liste_Mer_Plu, 'Ven_Mar': liste_Ven_Mar, 'Ven_Jup': liste_Ven_Jup, 'Ven_Sat': liste_Ven_Sat, 'Ven_Ura': liste_Ven_Ura, 'Ven_Nep': liste_Ven_Nep, 'Ven_Plu': liste_Ven_Plu, 'Mar_Jup': liste_Mar_Jup, 'Mar_Sat': liste_Mar_Sat, 'Mar_Ura': liste_Mar_Ura, 'Mar_Nep': liste_Mar_Nep, 'Mar_Plu': liste_Mar_Plu, 'Jup_Sat': liste_Jup_Sat, 'Jup_Ura': liste_Jup_Ura, 'Jup_Nep': liste_Jup_Nep, 'Jup_Plu': liste_Jup_Plu, 'Sat_Ura': liste_Sat_Ura, 'Sat_Nep': liste_Sat_Nep, 'Sat_Plu': liste_Sat_Plu, 'Ura_Nep': liste_Ura_Nep, 'Ura_Plu': liste_Ura_Plu, 'Nep_Plu': liste_Nep_Plu} 
-#dfObj = pd.DataFrame(dict)
-#flu = helio_asp[['Date']].copy()
-#concat_asp = pd.concat([flu, dfObj], axis=1)
-
-#asp_con = concat_asp[concat_asp.Date == today]
-#asp_con.round()
 # H_TrTr
 
 TrTr = TrTr.copy()
@@ -90,33 +65,6 @@ geo_TrTr = geo_TrTr.set_index('Date')
 geo_TrNa = TrNa[TrNa["Type"] == "Geo"]
 geo_TrNa = geo_TrNa[geo_TrNa.Date == today]
 geo_TrNa = geo_TrNa.set_index('Date')
-
-# GEO ASPECTS TrTr
-
-#ga = geo_asp_good
-
-#col = []
-#cols = ha.columns[1:]
-#df = ga[cols]
-#Date = list[ga['Date']]
-
-#for columns in df.columns:
-
-    #globals()["lizte_%s"%columns] = []
-
-    #for row, i in enumerate(df[columns]):
-        #if (i >= 0 and i <= 0.03) or (i >= 3.75 and i <= 3.755) or (i >= 7.5 and i <= 7.59) or (i >= 15 and i <= 15.15) or (i >= 22.5 and i <= 22.55) or (i >= 30 and i <= 30.15) or (i >= 36 and i <= 36.15) or (i >= 45 and i <= 45.15) or (i >= 60 and i <= 60.15) or (i >= 72 and i <= 72.15) or (i >= 90 and i <= 90.15) or (i >= 120 and i <= 120.15) or (i >= 135 and i <= 135.15) or (i >= 144 and i <= 144.15) or (i >= 150 and i <= 150.15) or (i >= 180 and i <= 180.15):
-        #    globals()["lizte_%s"%columns].append(i)
-        #else:
-          #  globals()["lizte_%s"%columns].append(" ")
-
-#flu1 = geo_asp_good[['Date']].copy()
-#dict = {'Sun_Mer': lizte_Sun_Mer, 'Sun_Ven': lizte_Sun_Ven, 'Sun_Mar': lizte_Sun_Mar, 'Sun_Jup': lizte_Sun_Jup, 'Sun_Sat': lizte_Sun_Sat, 'Sun_Ura': lizte_Sun_Ura, 'Sun_Nep': lizte_Sun_Nep, 'Sun_Plu': lizte_Sun_Plu, 'Mer_Ven': lizte_Mer_Ven, 'Mer_Mar': lizte_Mer_Mar, 'Mer_Jup': lizte_Mer_Jup, 'Mer_Sat': lizte_Mer_Sat, 'Mer_Ura': lizte_Mer_Ura, 'Mer_Nep': lizte_Mer_Nep, 'Mer_Plu': lizte_Mer_Plu, 'Ven_Mar': lizte_Ven_Mar, 'Ven_Jup': lizte_Ven_Jup, 'Ven_Sat': lizte_Ven_Sat, 'Ven_Ura': lizte_Ven_Ura, 'Ven_Nep': lizte_Ven_Nep, 'Ven_Plu': lizte_Ven_Plu, 'Mar_Jup': lizte_Mar_Jup, 'Mar_Sat': lizte_Mar_Sat, 'Mar_Ura': lizte_Mar_Ura, 'Mar_Nep': lizte_Mar_Nep, 'Mar_Plu': lizte_Mar_Plu, 'Jup_Sat': lizte_Jup_Sat, 'Jup_Ura': lizte_Jup_Ura, 'Jup_Nep': lizte_Jup_Nep, 'Jup_Plu': lizte_Jup_Plu, 'Sat_Ura': lizte_Sat_Ura, 'Sat_Nep': lizte_Sat_Nep, 'Sat_Plu': lizte_Sat_Plu, 'Ura_Nep': lizte_Ura_Nep, 'Ura_Plu': lizte_Ura_Plu, 'Nep_Plu': lizte_Nep_Plu}
-#dfObj = pd.DataFrame(dict)
-#concat_asp_g = pd.concat([flu1, dfObj], axis=1)
-
-#geo_con = concat_asp_g[concat_asp_g.Date == today]
-#geo_con.round()
 
 ### **III.2 Retro**
 
